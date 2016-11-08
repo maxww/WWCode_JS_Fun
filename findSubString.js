1e36903194ab10c5c28ada0139b3562b5d994736
maxww <tornglinchen@gmail.com>
2016-11-08 09:44:29 -0500

    added find sub str question

---
+++ findSubString.js
@@ -0,0 +1,19 @@
+/*
+Write a function that will check if a string contains the given substring,
+and return the index of the first letter of the substring.
+
+Examples:
+
+findSubStr('she has a cat.', 'cat')
+output => 10
+
+findSubStr('abcdde', 'de')
+output => 4
+
+findSubStr('happy holiday', 'a')
+output => 1
+*/
+
+function findSubStr (string, substring) {
+  // write your code here
+}
