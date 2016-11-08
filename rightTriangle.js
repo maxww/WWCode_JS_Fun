fddceb5e08cb02b83ddd2397b49df838da2fbe52
maxww <tornglinchen@gmail.com>
2016-11-08 09:40:04 -0500

    added right triangle question

---
+++ rightTriangle.js
@@ -0,0 +1,22 @@
+/*
+Write a function that will print out a right triangle with a given number
+
+Examples:
+
+rightTriangle(3)
+output => *
+          **
+          ***
+
+rightTriangle(5)
+output => *
+          **
+          ***
+          ****
+          *****
+*/
+
+
+function rightTriangle (number) {
+  // write your code here
+}
