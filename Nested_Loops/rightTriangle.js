97a53f0c6300c48a9a2d0c7327e64b640eec9681
Torng-Lin (Taffy) Chen <maxww@users.noreply.github.com>
2016-12-01 13:42:39 -0500

    added solution

--- Nested_Loops/rightTriangle.js
+++ Nested_Loops/rightTriangle.js
@@ -20,3 +20,19 @@ output => *
 function rightTriangle (number) {
   // write your code here
 }
+
+// Solution shown at the meet up with a few tiny changes
+function rightTriangle (number) {
+  let result = '';
+  for (let i=1; i<=number; i++) {
+    for (let j=1; j<=i; j++) {
+      result += '*'
+    }
+    if (i<number) {
+      result +='\n';
+    }
+  }
+  return result;
+}
+
+console.log(rightTriangle(3));
